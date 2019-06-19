c844096fa7aec6d4f53a1d164876a4e11a6b4158
Adrian Popan <adrian.popan@gmail.com>
2019-06-19 23:27:19 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,7 +2,7 @@ def dictionary
   dictionary = {:hello => "hi", :to => "2", :two => "2", :too => "2", :for => "4", :four => "4", :be => "b", :you => "u", :at => "@", :and => "&"}
 end
 
-def word_substituter
+def word_substituter(tweet_one)
   dictionary
   
 end
