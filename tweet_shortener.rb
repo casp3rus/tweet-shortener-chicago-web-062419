cb2075a5b86e8a6eb05b36090dd261d7e47b18bf
Adrian Popan <adrian.popan@gmail.com>
2019-06-19 23:39:42 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,12 +1,26 @@
 def dictionary
-  dictionary = {:hello => "hi", :to => "2", :two => "2", :too => "2", :for => "4", :four => "4", :be => "b", :you => "u", :at => "@", :and => "&"}
+  dictionary = {
+    "hello" => "hi", 
+    "to" => "2", 
+    "two" => "2", 
+    "too" => "2", 
+    "for" => "4", 
+    "four" => "4", 
+    "be" => "b", 
+    "you" => "u", 
+    "at" => "@", 
+    "and" => "&"
+  }
 end
 
-def word_substituter(tweet_one)
+def word_substituter(tweet)
   dictionary
-  
-  tweet = tweet_one.split
-  
+  tweet = tweet.split
+  dictionary.each do |word, replacement|
+    if tweet.include?(word)
+      
+    end 
+  end
 end
 
 def bulk_tweet_shortener
