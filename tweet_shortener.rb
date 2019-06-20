a1c085718add83a61af90d44c3dd337480143fb7
Adrian Popan <adrian.popan@gmail.com>
2019-06-20 00:03:54 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,7 +32,7 @@ def selective_tweet_shortener
   
 end
 
-def shortened_tweet_truncator
+def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
     word_substituter(tweet)[0..136] + "..."
   else
